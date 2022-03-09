0a498fd54d4186e4eada2269a0c4baf5ae4f87ec
TaoLoading <1377226380@qq.com>
2022-03-09 11:20:31 +0800

    搭建框架

---
+++ src/app/@shared/layouts/da-layout/default-layout.config.ts
@@ -0,0 +1,22 @@
+import { DaLayoutConfig } from './da-layout.type';
+
+export const DEFAULT_LAYOUT_CONFIG: DaLayoutConfig = {
+  id: "topNav",
+  mode: "headerTop",
+  header: {
+    fixed: true,
+    firHeader: {
+      height: 60
+    },
+    secHeader: {
+      hidden: true
+    }
+  },
+  sidebar: {
+    hidden: true,
+    shrink: false
+  },
+  footer: {
+    hidden: false
+  }
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './@core/core.module';
+import { SharedModule } from './@shared/shared.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule.forRoot(),
+    SharedModule.forRoot(),
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
