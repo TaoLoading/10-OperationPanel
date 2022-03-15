24886010931e8ae314c27771205292895c683684
TaoLoading <1377226380@qq.com>
2022-03-15 17:59:18 +0800

    新建charts1文件

--- src/app/pages/getting-started/getting-started.module.ts
+++ src/app/pages/getting-started/getting-started.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from 'src/app/@shared/shared.module';
 import { SampleComponent } from './sample/sample.component';
 import { GettingStartedComponent } from './getting-started.component';
 import { GettingStartedRoutingModule } from './getting-started-routing.module';
+import { Charts1Component } from './sample/charts1/charts1.component';
 
 @NgModule({
-  declarations: [GettingStartedComponent, SampleComponent],
+  declarations: [GettingStartedComponent, SampleComponent, Charts1Component],
   imports: [SharedModule, GettingStartedRoutingModule],
   providers: [],
 })
