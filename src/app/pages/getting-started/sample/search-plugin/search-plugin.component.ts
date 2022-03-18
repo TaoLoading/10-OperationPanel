417b961e2331b505a6603b5b8a1f2bfbe40178ed
TaoLoading <1377226380@qq.com>
2022-03-18 10:34:29 +0800

    增加插件的置顶、取消置顶、删除功能

--- src/app/pages/getting-started/sample/search-plugin/search-plugin.component.ts
+++ src/app/pages/getting-started/sample/search-plugin/search-plugin.component.ts
@@ -10,30 +10,42 @@ export class SearchPluginComponent implements OnInit {
   @Input() close;
 
   isFullScreen = false;
+  // 插件伪数据
   infoData = [
     {
+      id: 0,
       name: '插件1',
       description: '这是插件1',
       queryCriteria: 'sessionTime>=7200000',
-      createTime: '2022-03-15 17:11'
+      createTime: '2022-03-15 17:11',
+      isTop: false
     },
     {
+      id: 1,
       name: '插件2',
       description: '这是插件2',
       queryCriteria: 'bytes.out>bytes.in',
-      createTime: '2022-03-16 17:11'
+      createTime: '2022-03-16 17:11',
+      isTop: false
     },
     {
+      id: 2,
       name: '插件3',
       description: '这是插件3',
       queryCriteria: 'bytes.in==bytes.out',
-      createTime: '2022-03-17 17:11'
+      createTime: '2022-03-17 17:11',
+      isTop: false
     }
-  ]
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    // 读取localStorage中的插件数据
+    const pluginData = JSON.parse(localStorage.getItem('infoData'));
+    if (pluginData) {
+      this.infoData = pluginData;
+    }
   }
 
   toggleFullScreen() {
@@ -42,12 +54,44 @@ export class SearchPluginComponent implements OnInit {
   }
 
   // 删除插件
-  delete() {
-    console.log('点击了删除');
+  delete(item) {
+    for (let i = 0; i < this.infoData.length; i++) {
+      const element = this.infoData[i];
+      if (element === item) {
+        this.infoData.splice(i, 1);
+        // 将插件数据存储到localStorage中
+        localStorage.setItem('infoData', JSON.stringify(this.infoData));
+        return
+      }
+    }
   }
 
-  // 置顶插件
-  top() {
-    console.log('点击了置顶');
+  // 插件置顶与取消置顶
+  top(item) {
+    if (!item.isTop) {
+      // 置顶
+      for (let i = 0; i < this.infoData.length; i++) {
+        if (item === this.infoData[i]) {
+          item.isTop = true;
+          this.infoData.splice(i, 1);
+          this.infoData.unshift(item);
+          // 将插件数据存储到localStorage中
+          localStorage.setItem('infoData', JSON.stringify(this.infoData));
+          return
+        }
+      }
+    } else {
+      // 取消置顶
+      for (let i = 0; i < this.infoData.length; i++) {
+        if (item === this.infoData[i]) {
+          item.isTop = false;
+          this.infoData.splice(i, 1);
+          this.infoData.push(item);
+          // 将插件数据存储到localStorage中
+          localStorage.setItem('infoData', JSON.stringify(this.infoData));
+          return
+        }
+      }
+    }
   }
 }
